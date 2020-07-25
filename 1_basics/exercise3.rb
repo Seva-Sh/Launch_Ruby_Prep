de2abef03735693a555147f1274338a71a0dbd13
Vsevolod Shaposhnikov <vsevolod-shaposhnikov@hotmail.com>
2020-07-25 17:48:42 +0400

    Part 1

---
+++ 1_basics/exercise3.rb
@@ -0,0 +1,7 @@
+movie_hash = {:m1 => 1975, :m4 => 2004, :m5 => 2013, :m3 => 2001, :m2 => 1981}
+
+puts movie_hash[:m1]
+puts movie_hash[:m4]
+puts movie_hash[:m5]
+puts movie_hash[:m3]
+puts movie_hash[:m2]
---
+++ 1_basics/exercise5.rb
@@ -0,0 +1,15 @@
+num = 5
+
+puts num * (num - 1) * (num - 2) * (num - 3) * (num - 4)
+
+num = 6
+
+puts num * (num - 1) * (num - 2) * (num - 3) * (num - 4) * (num - 5)
+
+num = 7
+
+puts num * (num - 1) * (num - 2) * (num - 3) * (num - 4) * (num - 5) * (num - 6)
+
+num = 8
+
+puts num * (num - 1) * (num - 2) * (num - 3) * (num - 4) * (num - 5) * (num - 6) * (num - 7)
